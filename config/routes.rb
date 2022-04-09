17253df76391a6c0b1ea8ae7487ea1c4b9ad4708
Javiera Hidalgo <javiera_56@hotmail.com>
2022-04-09 03:55:38 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'translations#index'
+  post 'translations/translate', as: :translate
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+end
